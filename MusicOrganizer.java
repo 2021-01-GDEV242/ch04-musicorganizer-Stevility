ff969bce569ff66285e7780a9a1c2542f2906ab0
Stevility <65314652+Stevility@users.noreply.github.com>
2021-02-19 07:18:00 -0500

    Update Play tracks once random(IDK why the code moved right one space)

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -2,136 +2,149 @@ import java.util.ArrayList;
 //Genrates the ability to randomise
 import java.util.Random;
 /**
- * A class to hold details of audio tracks.
- * Individual tracks may be played.
- * 
- * @author David J. Barnes and Michael Kölling
- * @version 2016.02.29
- */
+* A class to hold details of audio tracks.
+* Individual tracks may be played.
+* 
+* @author David J. Barnes and Michael Kölling
+* @version 2016.02.29
+*/
 public class MusicOrganizer
 {
-    // An ArrayList for storing music tracks.
+        // An ArrayList for storing music tracks.
     private ArrayList<Track> tracks;
-    // A player for the music tracks.
+        // A player for the music tracks.
     private MusicPlayer player;
-    // A reader that can read music files and load them as tracks.
+        // A reader that can read music files and load them as tracks.
     private TrackReader reader;
-    
+        
     private Random shuffle;
-    
-    /**
-     * Create a MusicOrganizer
-     */
-    public MusicOrganizer()
-    {
-        tracks = new ArrayList<>();
-        player = new MusicPlayer();
-        reader = new TrackReader();
-        readLibrary("../audio");
-        System.out.println("Music library loaded. " + getNumberOfTracks() + " tracks.");
-        System.out.println();
-    }
-    
-    /**
-     * Add a track file to the collection.
-     * @param filename The file name of the track to be added.
-     */
-    public void addFile(String filename)
-    {
-        tracks.add(new Track(filename));
-    }
-    
-    /**
-     * Add a track to the collection.
-     * @param track The track to be added.
-     */
-    public void addTrack(Track track)
-    {
-        tracks.add(track);
-    }
-    
-    /**
-     * Play a track in the collection.
-     * @param index The index of the track to be played.
-     */
-    public void playTrack(int index)
-    {
-        if(indexValid(index)) {
-            Track track = tracks.get(index);
-            player.playSample(track.getFilename());
-            System.out.println("Now playing: " + track.getArtist() + " - " + track.getTitle());
+        
+        /**
+         * Create a MusicOrganizer
+         */
+        public MusicOrganizer()
+        {
+            tracks = new ArrayList<>();
+            player = new MusicPlayer();
+            reader = new TrackReader();
+            readLibrary("../audio");
+            System.out.println("Music library loaded. " + getNumberOfTracks() + " tracks.");
+            System.out.println();
         }
-    }
-    
-    /**
-     * Return the number of tracks in the collection.
-     * @return The number of tracks in the collection.
-     */
-    public int getNumberOfTracks()
-    {
-        return tracks.size();
-    }
-    
-    /**
-     * List a track from the collection.
-     * @param index The index of the track to be listed.
-     */
-    public void listTrack(int index)
-    {
-        System.out.print("Track " + index + ": ");
-        Track track = tracks.get(index);
-        System.out.println(track.getDetails());
-    }
-    
-    /**
-     * Show a list of all the tracks in the collection.
-     */
-    public void listAllTracks()
-    {
-        System.out.println("Track listing: ");
-
-        for(Track track : tracks) {
-            System.out.println(track.getDetails());
+        
+        /**
+         * Add a track file to the collection.
+         * @param filename The file name of the track to be added.
+         */
+        public void addFile(String filename)
+        {
+            tracks.add(new Track(filename));
         }
-        System.out.println();
-    }
-    
-    /**
-     * List all tracks by the given artist.
-     * @param artist The artist's name.
-     */
-    public void listByArtist(String artist)
-    {
-        for(Track track : tracks) {
-            if(track.getArtist().contains(artist)) {
-                System.out.println(track.getDetails());
+        
+        /**
+         * Add a track to the collection.
+         * @param track The track to be added.
+         */
+        public void addTrack(Track track)
+        {
+            tracks.add(track);
+        }
+        
+        /**
+         * Play a track in the collection.
+         * @param index The index of the track to be played.
+         */
+        public void playTrack(int index)
+        {
+            if(indexValid(index)) {
+                Track track = tracks.get(index);
+                player.playSample(track.getFilename());
+                System.out.println("Now playing: " + track.getArtist() + " - " + track.getTitle());
             }
         }
-    }
+        
+        /**
+         * Return the number of tracks in the collection.
+         * @return The number of tracks in the collection.
+         */
+        public int getNumberOfTracks()
+        {
+            return tracks.size();
+        }
+        
+        /**
+         * List a track from the collection.
+         * @param index The index of the track to be listed.
+         */
+        public void listTrack(int index)
+        {
+            System.out.print("Track " + index + ": ");
+            Track track = tracks.get(index);
+            System.out.println(track.getDetails());
+        }
+        
+        /**
+         * Show a list of all the tracks in the collection.
+         */
+        public void listAllTracks()
+        {
+            System.out.println("Track listing: ");
     
+            for(Track track : tracks) {
+                System.out.println(track.getDetails());
+            }
+            System.out.println();
+        }
+        
+        /**
+         * List all tracks by the given artist.
+         * @param artist The artist's name.
+         */
+        public void listByArtist(String artist)
+        {
+            for(Track track : tracks) {
+                if(track.getArtist().contains(artist)) {
+                    System.out.println(track.getDetails());
+                }
+            }
+        }
+        
+        /**
+         * Remove a track from the collection.
+         * @param index The index of the track to be removed.
+         */
+        public void removeTrack(int index)
+        {
+            if(indexValid(index)) {
+                tracks.remove(index);
+            }
+        }
+        
+        /**Shuffle Track
+         * 
+         */
+        public void shuffleTrack()
+        {
+            if(tracks.size() > 0) {
+               shuffle = new Random();
+               int index = shuffle.nextInt(tracks.size());
+               playTrack(index);
+            }
+        }
+        
     /**
-     * Remove a track from the collection.
-     * @param index The index of the track to be removed.
-     */
-    public void removeTrack(int index)
+     * Shuffle Play tracks once
+     */    
+    public void shuffleTrackonce()
     {
-        if(indexValid(index)) {
-            tracks.remove(index);
+        shuffle = new Random();
+        ArrayList<Track> nextTrack = new ArrayList<Track> (tracks);
+        while(nextTrack.size() > 0){
+                int index = shuffle.nextInt(nextTrack.size());
+                Track t = nextTrack.remove(index);
+                player.playSample(t.getFilename());
         }
     }
-    
-    /**Shuffle Track
-     * 
-     */
-    public void shuffleTrack()
-    {
-        if(tracks.size() > 0) {
-           shuffle = new Random();
-           int index = shuffle.nextInt(tracks.size());
-           playTrack(index);
-        }
-    }
-    
     /**
      * Play the first track in the collection, if there is one.
      */
